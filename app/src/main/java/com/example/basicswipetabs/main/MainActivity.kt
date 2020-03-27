e1397f0b9f785966d3623ad87a18d30c8d61dc20
tomaszmatusik <tomasz.matusik95@gmail.com>
2020-03-27 20:43:10 +0100

    tabs implemented

--- app/src/main/java/com/example/basicswipetabs/main/MainActivity.kt
+++ app/src/main/java/com/example/basicswipetabs/main/MainActivity.kt
@@ -2,27 +2,37 @@ package com.example.basicswipetabs.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.viewpager.widget.ViewPager
 import com.example.basicswipetabs.*
 import com.example.basicswipetabs.adapter.MainPagerAdapter
 import com.example.basicswipetabs.fragment.ListFragment
 import com.example.basicswipetabs.fragment.MainFragment
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
         setPagerAdapter()
     }
 
     fun setPagerAdapter(){
-        val pagerAdapter = MainPagerAdapter(supportFragmentManager)
-        pagerAdapter.addFragment(Constants.TAB_TITLE_MAIN, MainFragment())
-        pagerAdapter.addFragment(Constants.TAB_TITLE_LIST, ListFragment())
-        viewPager.adapter = pagerAdapter
+        val pagerAdapter = MainPagerAdapter(supportFragmentManager).apply {
+            addFragment(Constants.TAB_TITLE_MAIN, MainFragment())
+            addFragment(Constants.TAB_TITLE_LIST, ListFragment())
+        }
+
+        viewPager.apply {
+            adapter = pagerAdapter
+        }
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
